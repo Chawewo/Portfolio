d0067ede187cf98621012ff32bada185f0bf528c
Chawewo <davidtrujillo7982@gmail.com>
2022-12-08 22:41:13 -0800

    Making moves

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+// BEM -> Block Elemen Modifier
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="chawewo__navbar">
+      <div className="chawewo__navbar-links">
+        <div className="chawewo__navbar-links_logo">
+        Navbar
+        </div>
+      </div>
+    </div>
   )
 
 }
