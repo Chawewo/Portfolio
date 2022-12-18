2a952a08c53d314bf1cf1c8d237fc761fa61a3cd
chawewo@gmail.com <chawewo@gmail.com>
2022-12-18 03:32:15 -0800

    Implemented media queries

--- src/containers/whoChawewo/WhoChawewo.jsx
+++ src/containers/whoChawewo/WhoChawewo.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import './whoChawewo.css';
+/*import { SetStateAction } from 'react'; */
 const WhoChawewo = () => {
   return (
- <div className="container rounded text-center p-3" style={{width: '60%', background: "#191919", height: "100%"}} id="Who">
-  <div> 
-  <h1 style={{fontFamily: 'Recoleta',fontWeight: "10px", color: "#fff"}} className="gradient__text font-weight-bold">
+ <div className="chawewo__who section__padding" id="Who">
+  <div className= "chawewo__who-content" style={{zIndex: "0"}}>
+  <h1 className="gradient__text subtitle text-center">
+     Hello, my name is David.
+     </h1>
+     
+  <h5 className="gradient__text fade">
+I am a student at Fullerton College with a passion for becoming a full stack developer. I am currently enrolled in a computer science program at Fullerton College and have been working hard to develop my skills in programming.
+I have always been interested in technology and how it can be used to make a difference in the world. That's why I decided to pursue a career in full stack development. I am eager to learn as much as I can about all aspects of web development, from front-end design to back-end programming.
+<br/>
+<br/>
+In addition to my coursework, I have also been actively learning on my own through online resources and tutorials. I am particularly interested in full stack development and have been focusing on learning languages such as HTML, CSS, and JavaScript. I have also dabbled in server-side languages like SQL and Python
+
+<br/>
+<br/>
+I am driven and self-motivated, and I am always looking for opportunities to challenge myself and improve my skills. In the future, I hope to use my knowledge and expertise to create innovative and useful applications that solve real-world problems.
+I am excited to continue my journey as a full stack developer and make a positive impact through my work. Thank you for visiting my website. Feel free to reach out to me if you have any questions or just want to chat about technology and programming.
+  </h5>
+  
+  </div>
+  </div>
+  )
+}
+
+export default WhoChawewo
+/*
+ <h6 style={{fontFamily: 'Recoleta', color: "#fff", overflow:'auto'}} className="font-weight-light text-center ">
+I am currently a student at Fullerton college studying Computer Science and plan to transfer to Cal State Fullerton this Fall Semester.
+Currently striving to become a Full-Stack software engineer to unleash my creativity, taking it one step at a time. 
+
+ </h6>
+
+ Second 
+
+ <div className= "chawewo__content"> 
+  <h1 className="chawewo__container gradient__text"> {/*style={{fontFamily: 'Recoleta',fontWeight: "10px", color: "#fff"}} className="gradient__text font-weight-bold text-center"
     Hello, my name is David.
   </h1>
  <h6 style={{fontFamily: 'Recoleta', color: "#fff", overflow:'auto'}} className="font-weight-light text-center ">
@@ -17,14 +52,4 @@ I am excited to continue my journey as a full stack developer and make a positiv
  </h6>
  </div>
  </div>
-  )
-}
-
-export default WhoChawewo
-/*
- <h6 style={{fontFamily: 'Recoleta', color: "#fff", overflow:'auto'}} className="font-weight-light text-center ">
-I am currently a student at Fullerton college studying Computer Science and plan to transfer to Cal State Fullerton this Fall Semester.
-Currently striving to become a Full-Stack software engineer to unleash my creativity, taking it one step at a time. 
-
- </h6>
  */
