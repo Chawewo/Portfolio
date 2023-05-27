fbe7237a0f1acb89caa0108fb0ba6d6ee73abfce
Chawewo <davidtrujillo7982@gmail.com>
2023-05-27 16:20:33 -0700

    Begun development of projects component

--- frontend/src/containers/project/Project.jsx
+++ frontend/src/containers/project/Project.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
 import './project.css';
-const Project = () => {
+import React, { useState } from 'react';
+
+const projects = [
+  {
+  id: 1,
+  title: "Cache-Sim",
+  description: "This is a cache simulator",
+  date: "2023-05-24",
+  link: "#",
+  image: "path to image"
+  }
+];
+
+function ProjectItem({ project, onProjectClick, isSelected, onClose }) {
+  const className = `project ${isSelected ? 'project-selected' : ''}`;
+
   return (
-    <div id="projects">Project</div>
-  )
+    <div className={className} onClick={() => onProjectClick(project)}>
+      {isSelected && <button onClick={onClose} className="close-button">X</button>}
+      <img src={project.image} alt={project.title} className="project-image" />
+      <h2>{project.title}</h2>
+      {isSelected && <p>{project.description}</p>}
+    </div>
+  );
 }
 
-export default Project
+function Projects({ projects }) {
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleProjectClick = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleClose = (event) => {
+    event.stopPropagation();
+    setSelectedProject(null);
+  };
+
+  return (
+    <div className="projects">
+      {projects.map((project) => (
+        <ProjectItem
+          key={project.id}
+          project={project}
+          onProjectClick={handleProjectClick}
+          isSelected={project === selectedProject}
+          onClose={handleClose}
+        />
+      ))}
+    </div>
+  );
+}
+
+const Project = () => {
+  return (
+    <div className="project-timeline">
+      <Projects projects={projects} />
+    </div>
+  );
+};
+
+export default Project;
