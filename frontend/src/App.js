c8d51df4ca943c6688d6d6fecf61c16bedbd4f08
David <Chawewo@gmail.com>
2024-09-03 13:21:26 -0700

    readded node modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
-    <Background/>
+     <Background/>
     <Navbar />
     <Routes>
         <Route exact path="/" element={<Header />} />
