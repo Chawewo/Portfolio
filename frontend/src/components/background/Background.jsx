c7d631201a0fdcf58ef0ab5aa97c8f66f02cabac
David <Chawewo@gmail.com>
2024-09-03 13:45:40 -0700

    fixed errors

--- frontend/src/components/background/Background.jsx
+++ frontend/src/components/background/Background.jsx
@@ -3,7 +3,7 @@ import background from '../../assets/background.jpg';
 import './background.css';
 
 const Background = () => {
-  const [windowDimensions, setWindowDimensions] = useState({
+  const [setWindowDimensions] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
